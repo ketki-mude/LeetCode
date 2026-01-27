374bdf617b08a9ae7a0b5f6ba56a10fc831a091f
Ketki Mude <132642080+ketki-mude@users.noreply.github.com>
2026-01-27 17:15:22 -0500

    Time: 7 ms (82.59%), Space: 23.6 MB (6.24%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,14 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
-        clean_string=""
-        for char in s:
-            if char.isalnum():
-                clean_string+= char.lower()
+        # clean_string=""
+        # for char in s:
+        #     if char.isalnum():
+        #         clean_string+= char.lower()
+        clean_s = "".join([c.lower() for c in s if c.isalnum()])
+        reverse_string=clean_s[::-1]
         
-        reverse_string=clean_string[::-1]
-        
-        if reverse_string==clean_string:
+        if reverse_string==clean_s:
             return True
         
         return False
