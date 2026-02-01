d5e01f735b50341606321fc371f2704833cb156e
Ketki Mude <132642080+ketki-mude@users.noreply.github.com>
2026-01-31 22:50:33 -0500

    Time: 31 ms (13.39%), Space: 19.6 MB (34.77%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        
+        magazine_count={}
+
+        for char in magazine:
+            if char in magazine_count:
+                magazine_count[char]+=1
+            else:
+                magazine_count[char]=1
+
+        for char in ransomNote:
+            if char not in magazine_count or magazine_count[char]==0:
+                return False
+            else:
+                magazine_count[char]-=1
+
+        return True
+
+
+        
