feeefffd01370c7ca0c7dc2087ffcbe49f3ba5e3
Ketki Mude <132642080+ketki-mude@users.noreply.github.com>
2026-08-27 17:24:25 -0400

    Time: 4 ms (41.93%), Space: 20.6 MB (18.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+        unique_dict={}
 
-        unique_dict = {}
+        for i in range(0,len(nums)):
+            unique_dict[nums[i]]= i
 
-        for i in range(0, len(nums)):
-            unique_dict[nums[i]] = i
+        for i in range(0,len(nums)):
+            key= target - nums[i]
 
-        for i in range (0, len(nums)):
-            key = target - nums[i]
+            if key in unique_dict and i!= unique_dict[key]:
+                return (i,unique_dict[key])
 
-            if key in unique_dict and i !=unique_dict[key]:
-                return [i, unique_dict[key]]
+        
+
+
 
 
 
