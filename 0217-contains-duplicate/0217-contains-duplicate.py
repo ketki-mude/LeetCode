2807a8e56881953454f997ae47eaa19a8f64ee9b
Ketki Mude <132642080+ketki-mude@users.noreply.github.com>
2026-08-27 16:32:37 -0400

    Time: 15 ms (63.88%), Space: 37.3 MB (7.66%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -3,18 +3,13 @@ class Solution:
         unique_nums={}
 
         for num in nums:
-            if num not in unique_nums:
-                unique_nums[num]=1
-            else:
-                unique_nums[num] += 1
-
-        for num in unique_nums:
-            if unique_nums[num]>1:
-                return True
-            else:
-                continue
-           
-        return False
-
-            # {2:1,14:1,18:1,22:2}
+            unique_nums[num]= None
         
+        if len(nums)==len(unique_nums):
+            return False
+        else:
+            return True
+         
+
+
+      
