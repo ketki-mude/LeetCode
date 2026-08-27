cf1ec8c2a05e5738bfa9da2da1a6e900d072c4e6
Ketki Mude <132642080+ketki-mude@users.noreply.github.com>
2026-08-27 18:09:07 -0400

    Time: 10 ms (79.65%), Space: 19.5 MB (29.63%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -12,13 +12,19 @@ class Solution:
                 s_dict[char]+=1
             else:
                 s_dict[char]=1
-        
+
+        print(s_dict)
+
         for char in t:
             if char in t_dict:
                 t_dict[char]+=1
             else:
                 t_dict[char]=1
-        
+
+        print(t_dict)
+
+    
+
         for char in s_dict:
             if char in t_dict:
                 if s_dict[char]!=t_dict[char]:
@@ -28,7 +34,11 @@ class Solution:
             
         return True
 
-        
+    
+
+
+
+
 
 
         
