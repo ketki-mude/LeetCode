b8a63df27c9f45c8d45e5985b6715e7f1a9a6f49
Ketki Mude <132642080+ketki-mude@users.noreply.github.com>
2026-09-05 17:59:29 -0400

    Time: 80 ms (84.67%), Space: 31.1 MB (87.62%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -4,21 +4,27 @@ class MinStack:
         self.stack=[]
         self.min_stack=[]
         
+
     def push(self, value: int) -> None:
+
         self.stack.append(value)
 
         if not self.min_stack:
             self.min_stack.append(value)
         else:
-            self.min_stack.append(min(value,self.min_stack[-1]))     
+            self.min_stack.append(min(value,self.min_stack[-1]))
+        
 
     def pop(self) -> None:
         self.stack.pop()
         self.min_stack.pop()
         
+
     def top(self) -> int:
+
         return self.stack[-1]
         
+
     def getMin(self) -> int:
         return self.min_stack[-1]
         
